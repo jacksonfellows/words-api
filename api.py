b061be21faf32558fb2073958f1c32fe21935201
Jackson Fellows <Jfellows@jfellows-a01.vmware.com>
2018-06-24 17:35:59 -0700

    bugfixes (many)

--- api.py
+++ api.py
@@ -1,19 +1,33 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 from flask_restful import Resource, Api
-import pickle, random
+import pickle, random, json
 from load_words import load_words
 
 app = Flask(__name__)
 api = Api(app)
 
 class Words(Resource):
+    def options (self):
+        return {'Allow': 'POST'}, 200, {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': '*'
+            }
+
     def post(self):
-        num_words = int(request.form['num'])
-        words = []
-        while num_words > 0:
-            words.append(words_list[random.randrange(0, stop=len(words_list))])
-            num_words -= 1
-        return {'words': words}
+        try:
+            num_words = int(request.get_json()['num'])
+        except:
+            resp = Response(json.dumps({'error': 'Could not interpret the request {}.'.format(request)}), status=400)
+        else:
+            words = []
+            while num_words > 0:
+                words.append(words_list[random.randrange(0, stop=len(words_list))])
+                num_words -= 1
+            resp = Response(json.dumps({'words': words}))
+        finally:
+            resp.headers['Access-Control-Allow-Origin'] = '*'
+            return resp
         
 
 api.add_resource(Words, '/')
